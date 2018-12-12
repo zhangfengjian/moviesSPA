5c6240569bf7aca58cd21cb05eb8d91105e6d629
zhangfengjian <1845208711@qq.com>
2018-12-12 11:34:09 +0800

    修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: 'vueblog',
+      name: 'hpapp',
       script: 'build/main.js',
+      exec_mode: 'cluster',
+      instances: 'max',
       env: {
-        COMMON_VARIABLE: 'true'
+        PORT: 3000,
+        NODE_CONFIG: 'dev',
+        NODE_ENV: 'development'
       },
-      env_production: {
+      env_test: {
+        PORT: 3000,
+        NODE_CONFIG: 'test',
+        NODE_ENV: 'production'
+      },
+      env_pre: {
+        PORT: 3000,
+        NODE_CONFIG: 'pre',
+        NODE_ENV: 'production'
+      },
+      env_prod: {
+        PORT: 3000,
+        NODE_CONFIG: 'prod',
         NODE_ENV: 'production'
       }
     }
@@ -14,10 +30,10 @@ module.exports = {
   deploy: {
     production: {
       user: 'root',
-      host: '', // 服务器IP
+      host: '47.107.250.36', // 服务器IP
       ref: 'origin/master',
-      repo: 'git@github.com:wmui/vueblog.git',
-      path: '/www/vueblog',
+      repo: 'git@github.com:zhangfengjian/moviesSPA.git',
+      path: '/var/www/moviesSPA',
       'post-deploy': 'yarn && npm run build && pm2 reload ecosystem.config.js --env production'
     }
   }
